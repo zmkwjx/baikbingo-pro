540201e0f454d7a672b6aba4c2d08d12f80e2a95
“zhouwei” <zhouwei@cn-ds.com.cn>
2021-12-22 17:03:47 +0800

    feat / 登录

--- src/page/login/index.vue
+++ src/page/login/index.vue
@@ -6,33 +6,35 @@
     <div class="bkpro-login-wrapper" :class="{ 'show-wrapper': loading }">
       <div class="bkpro-login-wrapper-box">
         <div class="bkpro-login-wrapper-box__inner">
-          <h1>baikbingo</h1>
+          <h1>Baikbingo Pro</h1>
           <p>
             baikbingo福利社开源的一套saas框架，框架集成element-plus、vuex4、particles.vue3等开源组件
           </p>
         </div>
-        <div class="bkpro-login-wrapper-box__content"></div>
+        <div class="bkpro-login-wrapper-box__content">
+          <div class="bkpro-login-main">
+            <div class="bkpro-login-logo">
+              <h2>Baikbingo 福利社</h2>
+            </div>
+            <div class="bkpro-login-type">
+              <h3>账户登录</h3>
+            </div>
+            <user-login></user-login>
+          </div>
+        </div>
       </div>
       <!-- <div @click="login">登录</div> -->
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-// import { useRouter } from "vue-router";
-// import { useStore } from "@/store";
 import { ref } from "vue";
 import options from "./options.json";
+import userLogin from "./userLogin.vue";
 
 // 缓存
 const loading = ref(false);
 setTimeout(() => {
   loading.value = true;
 }, 100);
-
-// const store = useStore();
-// const router = useRouter();
-const login = () => {
-  // store.commit("SET_TOKEN", "dddddd");
-  // router.push({ path: "/" });
-};
 </script>
---
+++ src/page/login/userLogin.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="bkpro-login-form">
+    <el-form
+      ref="loginRef"
+      :model="loginForm"
+      :rules="loginRules"
+      status-icon
+      label-width="0"
+    >
+      <el-form-item prop="username">
+        <el-input
+          @keyup.enter.native="handleLogin"
+          v-model="loginForm.username"
+          auto-complete="off"
+          placeholder="请输入用户名"
+        >
+          <template #prefix>
+            <i class="iconfont icon-user" s></i>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          @keyup.enter.native="handleLogin"
+          v-model="loginForm.password"
+          auto-complete="off"
+          type="password"
+          placeholder="请输入密码"
+        >
+          <template #prefix>
+            <i class="iconfont icon-lock_fill" style="font-size: 18px"></i>
+          </template>
+        </el-input>
+      </el-form-item>
+    </el-form>
+    <!-- 提交 -->
+    <div class="bkpro-login-submit">
+      <el-button @click.native.prevent="handleLogin">登录</el-button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { useRouter } from "vue-router";
+import { defineComponent, ref, onMounted } from "vue";
+import { useStore } from "@/store";
+
+export default defineComponent({
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      loginRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 1, message: "密码长度最少为6位", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  setup() {
+    const loginRef = ref();
+    const store = useStore();
+    const router = useRouter();
+    const handleLogin = () => {
+      loginRef.value.validate((valid: any) => {
+        if (valid) {
+          store.commit("SET_TOKEN", "dddddd");
+          router.push({ path: "/" });
+        }
+      });
+    };
+    return {
+      loginRef,
+      handleLogin
+    };
+  }
+});
+</script>
